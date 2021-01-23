8c02cf96c7eb8208a41aa17ab0a852d8df7773e9
Davide Risso <risso.davide@gmail.com>
2021-01-23 17:02:00 +0100

    Added some minimal tests

--- tests/testthat/test-pareto_norm.R
+++ tests/testthat/test-pareto_norm.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("PSiNorm works with all input classes", {
+  m <- matrix(c(1,0,2,0,2,9,3,0), ncol=2)
+  
+  out1 <- PsiNorm(m)
+  
+  se <- SummarizedExperiment(m)
+  sce <- SingleCellExperiment(m)
+  
+  out_se <- PsiNorm(se)
+  expect_equal(out1, assay(out_se, "PsiNorm"))
+  
+  out_sce <- PsiNorm(sce)
+  out2 <- t(t(m)/sizeFactors(out_sce))
+  expect_equal(out1, out2)
 })
